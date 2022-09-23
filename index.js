860f7f63d83477eeace02cf085cab2f5093f9d2d
Vinicius Benso <vinibenso@gmail.com>
2022-09-23 13:55:06 -0300

    organizando codigo

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-const { ApolloServer, gql } = require ('apollo-server')
+const { ApolloServer, gql } = require('apollo-server')
 
-const typeDefs =gql`
-  #scalar Date para poder retornar um Date ja que no graphql nao aceita.
+const perfis = [
+  { id: 1, name: 'comum', }, { id: 2, name: "Administrador" }]
+
+const users = [{
+  id: 1,
+  name: 'Joao Silva',
+  email: 'jsilva@gmail.com',
+  age: 29,
+  perfil_id: 1
+}, {
+  id: 2,
+  name: 'Rafae Junior',
+  email: 'rjunior@gmail.com',
+  age: 18,
+  perfil_id: 2
+}, {
+  id: 3,
+  name: 'Daniele Brito',
+  email: 'dbrito@gmail.com',
+  age: 35,
+  perfil_id: 1
+}]
+
+const typeDefs = gql`
+
   scalar Date
-
-  #criando tipo/ a exclamcao indica q é obrigatorio
-  type User {
-    id: ID
-    name: String!
-    email: String!
-    age: Int
-    salary: Float
-    vip: Boolean
-  }
 
   type Product {
     name: String!
@@ -21,26 +34,58 @@ const typeDefs =gql`
     priceWithDiscount: Float
   }
 
+  type Perfil {
+  id: Int
+  name: String!
+  }
+
+  type User {
+   id: ID
+   name: String!
+   email: String!
+   age: Int
+   salary: Float
+   vip: Boolean
+   perfil: Perfil
+ }
+
   #Pontos de entrada da sua API!
   type Query {
     ola: String
     horaAtual:  Date
     loggedUser: User
     featuredProduct: Product
-
+    numerosMegaSena: [Int!]!
+    users: [User]
+    user(id: ID): User
+    perfis: [Perfil]
+    perfil(id: Int): Perfil
+    
   }
-`
+
+  `
 
 const resolvers = {
 
   Product: {
     priceWithDiscount(product) {
-      if(product.discount){
+      if (product.discount) {
         return product.price
           * (1 - product.discount)
       } else {
         return product.price
       }
+    }
+  },
+
+  User: {
+    salary(user) {
+      return user.real_salary
+    },
+    perfil(user) {
+      const select = perfis
+        .filter(p => p.id === user.perfil_id)
+      return select ? select[0] : null
     }
   },
 
@@ -66,18 +111,46 @@ const resolvers = {
         name: "mouse",
         price: 247.55,
         discount: 0.15,
-        
-        
+
+
       }
-    } 
+    },
+    numerosMegaSena() {
+      // return [4, 8, 13, 27, 33, 54]
+      const crescente = (a, b) => a - b
+      return Array(6).fill(0)
+        .map(n => parseInt(Math.random() * 60 + 1))
+        .sort(crescente)
+    },
+    users() {
+      return users
+    },
+    user(_, args) {
+      const select = users
+        .filter(u => u.id == args.id)
+      return select ? select[0] : null
+
+    },
+
+    perfis() {
+      return perfis
+    },
+
+    perfil(_, { id }) {
+      const select = perfis
+        .filter(p => p.id === id)
+      return select ? select[0] : null
+
+
+    }
   }
 }
 
 const server = new ApolloServer({
-  typeDefs, 
+  typeDefs,
   resolvers
 })
 
-server.listen().then(({url}) =>{
+server.listen().then(({ url }) => {
   console.log(`Executando em ${url}`)
 })
