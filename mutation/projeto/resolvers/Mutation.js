8c94892527f84c95f05063bab1c5772150b87f1f
Vinicius Benso <vinibenso@gmail.com>
2022-09-25 17:05:46 -0300

    finished mutation module

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -1,17 +1,31 @@
 const { usuarios, proximoId } = require('../data/db')
 
+
+function indiceUsuario(filtro) {
+  if (!filtro) return -1
+  const { id, email } = filtro
+  if (id) {
+    return usuarios
+      .findIndex(u => u.id === id)
+  } else if (email) {
+    return usuarios
+      .findIndex(u => u.email === email)
+  }
+  return -1
+}
+
 module.exports = {
   //{nome, email, idade}
-  novoUsuario(_, args) {
+  novoUsuario(_, { dados }) {
     const emailExistente = usuarios
-      .some(u => u.email === args.email)
+      .some(u => u.email === dados.email)
 
     if (emailExistente) {
       throw new Error('E-mail já cadastrado')
     }
     const novo = {
       id: proximoId(),
-      ...args,
+      ...dados,
       perfil_id: 1,
       status: 'ATIVO'
     }
@@ -19,9 +33,9 @@ module.exports = {
     usuarios.push(novo)
     return novo
   },
-  excluirUsuario(_, { id }) {
-    const i = usuarios
-      .findIndex(u => u.id === id)
+
+  excluirUsuario(_, { filtro }) {
+    const i = indiceUsuario(filtro)
     if (i < 0) return null
     const excluidos = usuarios.splice(i, 1)
     return excluidos ? excluidos[0] : null
